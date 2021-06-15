c69ffa24bff071e090d9c49b6930c9f28b30f392
MariAuxiHer <72420601+MariAuxiHer@users.noreply.github.com>
2021-06-15 16:15:25 -0500

    Deleted logo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+
       <header className="App-header">
       Dictionary App
       </header>
